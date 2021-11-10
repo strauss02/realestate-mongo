20e6a2a4210bc5138c8f6c87062b22ab64777e7d
Ido Strauss <strauss02@hotmail.co.il>
2021-11-10 15:49:18 +0200

    Revert "add csv agents"
    This reverts commit 0d920ff08d3a4ae7227d128059a5799efeffaa7d.

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require('path')
+const app = new express()
 
-const URL = `mongodb+srv://ido:dozen12@cluster0.8yoes.mongodb.net/realestatedb?retryWrites=true&w=majority`
+app.use(cors)
 
-app.use(cors())
-app.use(express.json())
+// mongoose.connect(URL, { useNewUrlParse: true }) //Check reason
 
-mongoose.connect(URL, { useNewUrlParser: true }) //Check reason
-
-app.get('/', (req, res) => {
-  console.log(req)
+app.get('http://localhost:8080/', (req, res) => {
   res.json('allah')
 })
 
